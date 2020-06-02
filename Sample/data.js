768493f8c6d7144e0fa01cc60af1de77dfc0f316
fantomx11 <fantomx11@gmail.com>
2020-06-02 11:29:03 -0500

    Sample

---
+++ Sample/data.js
@@ -0,0 +1,355 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pre-mitigation---bedroom",
+      "name": "Pre-Mitigation - Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5240203320257066,
+          "pitch": 0.09750356993984255,
+          "rotation": 0,
+          "target": "1-pre-mitigation---living-room"
+        },
+        {
+          "yaw": 0.73357267390411,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "4-post-demo---bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pre-mitigation---living-room",
+      "name": "Pre-Mitigation - Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4097822620492941,
+          "pitch": 0.0847794355367899,
+          "rotation": 0,
+          "target": "3-pre-mitigation---dining-room"
+        },
+        {
+          "yaw": 2.0147459115234643,
+          "pitch": 0.1214639386691303,
+          "rotation": 0,
+          "target": "0-pre-mitigation---bedroom"
+        },
+        {
+          "yaw": 0.29060406384929216,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "5-post-demo---living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pre-mitigation---kitchen",
+      "name": "Pre-Mitigation - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8987975566748716,
+          "pitch": 0.17323473319111216,
+          "rotation": 0,
+          "target": "3-pre-mitigation---dining-room"
+        },
+        {
+          "yaw": 0.1556721090891795,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "6-post-demo---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pre-mitigation---dining-room",
+      "name": "Pre-Mitigation - Dining Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.426094763231024,
+          "pitch": 0.28940413588554037,
+          "rotation": 0,
+          "target": "1-pre-mitigation---living-room"
+        },
+        {
+          "yaw": 2.2346355638929527,
+          "pitch": 0.22764136788510037,
+          "rotation": 0,
+          "target": "2-pre-mitigation---kitchen"
+        },
+        {
+          "yaw": 0.219381869621337,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "0-post-demo---dining-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-post-demo---bedroom",
+      "name": "Post-Demo - Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.156464317700033,
+          "pitch": 1.4817671215661345,
+          "rotation": 6.283185307179586,
+          "target": "0-pre-mitigation---bedroom"
+        },
+        {
+          "yaw": 1.5204150967428536,
+          "pitch": 0.26442196216551217,
+          "rotation": 0,
+          "target": "5-post-demo---living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-post-demo---living-room",
+      "name": "Post-Demo - Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2588187496078973,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "1-pre-mitigation---living-room"
+        },
+        {
+          "yaw": 1.4133186022974513,
+          "pitch": 0.11049178977198437,
+          "rotation": 0,
+          "target": "0-post-demo---dining-room"
+        },
+        {
+          "yaw": 2.005944547813635,
+          "pitch": 0.14656521149857582,
+          "rotation": 0,
+          "target": "4-post-demo---bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-post-demo---kitchen",
+      "name": "Post-Demo - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9012129938635827,
+          "pitch": 0.22847993230832842,
+          "rotation": 0,
+          "target": "0-post-demo---dining-room"
+        },
+        {
+          "yaw": -1.4480416345858824,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "2-pre-mitigation---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "0-post-demo---dining-room",
+      "name": "Post-Demo - Dining Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7009536760769368,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "3-pre-mitigation---dining-room"
+        },
+        {
+          "yaw": -2.421392308809967,
+          "pitch": 0.3180286061795652,
+          "rotation": 0,
+          "target": "5-post-demo---living-room"
+        },
+        {
+          "yaw": 2.222353013151274,
+          "pitch": 0.3113871965814141,
+          "rotation": 0,
+          "target": "6-post-demo---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
