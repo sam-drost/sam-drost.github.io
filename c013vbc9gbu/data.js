ccaa02452be11db36575dbbabff50fdd5b68ef8d
fantomx11 <fantomx11@gmail.com>
2020-06-03 15:47:05 -0500

    1 job

---
+++ c013vbc9gbu/data.js
@@ -0,0 +1,325 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entry-12",
+      "name": "Entry (1/2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.6502369273851052,
+        "pitch": 0.3605892371818449,
+        "fov": 1.3396805147900568
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6904962304043227,
+          "pitch": 0.5470980933309004,
+          "rotation": 0,
+          "target": "1-entry-22"
+        },
+        {
+          "yaw": -2.3575337331925326,
+          "pitch": 0.31042636482251496,
+          "rotation": 0,
+          "target": "3-sitting-room-12"
+        },
+        {
+          "yaw": 1.0681197237965154,
+          "pitch": 0.561183455622519,
+          "rotation": 0,
+          "target": "5-dining-room-12"
+        },
+        {
+          "yaw": 0.9321454011099757,
+          "pitch": 0.3003547390141925,
+          "rotation": 0,
+          "target": "6-dining-room-22"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entry-22",
+      "name": "Entry (2/2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1661775103610275,
+        "pitch": 0.06919652496290851,
+        "fov": 1.3396805147900568
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.370671906027031,
+          "pitch": 0.5175641360490797,
+          "rotation": 0,
+          "target": "0-entry-12"
+        },
+        {
+          "yaw": 2.9565231499056637,
+          "pitch": 0.707118836021797,
+          "rotation": 0,
+          "target": "2-butlers-pantry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-butlers-pantry",
+      "name": "Butler's Pantry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.13005517974539416,
+        "pitch": 0.3662265957538935,
+        "fov": 1.3396805147900568
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04921650065809047,
+          "pitch": 0.7358252794430697,
+          "rotation": 0,
+          "target": "1-entry-22"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sitting-room-12",
+      "name": "Sitting Room (1/2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.2021736985116522,
+        "pitch": 0.31576405375457384,
+        "fov": 1.3396805147900568
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8800946742597473,
+          "pitch": 0.4559183577595487,
+          "rotation": 0,
+          "target": "4-sitting-room-22"
+        },
+        {
+          "yaw": 2.856045383229132,
+          "pitch": 0.29066800287430183,
+          "rotation": 0,
+          "target": "0-entry-12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sitting-room-22",
+      "name": "Sitting Room (2/2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.9553675172600897,
+        "pitch": 0.3763720739286427,
+        "fov": 1.3396805147900568
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1136025763589537,
+          "pitch": 0.6739345600557876,
+          "rotation": 0,
+          "target": "2-butlers-pantry"
+        },
+        {
+          "yaw": -2.1242363700863507,
+          "pitch": 0.492684219586792,
+          "rotation": 0,
+          "target": "3-sitting-room-12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dining-room-12",
+      "name": "Dining Room (1/2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.453709893628414,
+        "pitch": 0.3567943461826566,
+        "fov": 1.3396805147900568
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3144664729122937,
+          "pitch": 0.6181288629011217,
+          "rotation": 0,
+          "target": "0-entry-12"
+        },
+        {
+          "yaw": 0.6281297397729624,
+          "pitch": 0.48031802949383895,
+          "rotation": 0,
+          "target": "6-dining-room-22"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dining-room-22",
+      "name": "Dining Room (2/2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.081304709482321,
+        "pitch": 0.15879084844358005,
+        "fov": 1.3396805147900568
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0466119275290797,
+          "pitch": 0.5119337868945237,
+          "rotation": 0,
+          "target": "5-dining-room-12"
+        },
+        {
+          "yaw": -2.872764579585091,
+          "pitch": 0.27557968767822416,
+          "rotation": 0,
+          "target": "0-entry-12"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
